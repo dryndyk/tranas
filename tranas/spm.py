f7be5cd21ed8f11de70716eaa17b9dd1c6baa8a8
Dmitry Ryndyk <dmitry.ryndyk@tu-dresden.de>
2026-05-12 06:34:24 +0200

    temporary

---
+++ tranas/spm.py
@@ -0,0 +1,45 @@
+"""This module defines TraNaS classes for Scanning Probe Microscopy (SPM).
+
+Author: Dmitry A. Ryndyk <dmitry.ryndyk@tu-dresden.de>
+"""
+
+
+class SPM:
+    """ TraNaS class for SPM.
+    
+    """    
+    
+    
+    def __init__(self):
+        
+        self.atoms = None
+        
+    def shift(self, x,y,z):
+        """Shifts the tip coordinates."""
+        
+        atoms = self.atoms
+        for n in range(self.tip1-1,self.tip2):
+            atoms.positions[n] = self.atoms.positions[n]+[x,y,z]
+        
+        return atoms
+    
+    def point(self, x,y,z):
+        """Calculates STM for given tip coordinates."""
+        
+        # Change parameters of "atoms" object.   
+        atoms = self.shift(x,y,z)
+        
+        # Make CP2K input file from "inp" and "atoms" objects.
+        precalc = self.precalc
+        precalc.atoms = atoms
+        inp_file = precalc._generate_input()
+        inp_fname = precalc.label + '.inp'
+        file = open(inp_fname,'w')
+        file.write(inp_file)
+        file.close()
+        from ase.io import write
+        write(precalc.label+'.xyz', atoms)
+
+        # Start CP2K calculation. 
+        from subprocess import check_call
+        check_call(['mpirun', '-n', '8', 'cp2k', inp_fname])
